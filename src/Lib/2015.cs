2c4cc347a3a01cea6ecb9c565fc8b31be6c131bf
janmaru <janmaru@gmail.com>
2020-05-18 00:05:32 +0200

    first day 2015

--- src/Lib/2015.cs
+++ src/Lib/2015.cs
@@ -21,7 +21,7 @@ namespace Mahamudra.AdventOfCode.Core
         public static int DayOneWhatFloorv2BigO2ɛn(string input)  // BigOɛn linear x 2, hence again linear. The running time grows no faster than a constant times nnn
         {
             var up = input.ToCharArray().Where(x => x == ')').Sum(x => -1);
-            return input.Length + 2*up; 
+            return input.Length + 2 * up;
         }
 
         public static int DayOneWhatFloorv3BigO2ɛn(string input)  // BigOɛn linear x 2, hence again linear. The running time grows no faster than a constant times nnn
@@ -38,5 +38,28 @@ namespace Mahamudra.AdventOfCode.Core
             }
             return count;
         }
+
+
+        public static int DayTwoWhatFloorBigO2ɛn(string input)
+        {
+            var symbols = input.ToCharArray();  //  number of char as the Length of the input string
+            var count = 0;
+            var index = 0;
+            foreach (var s in symbols)
+            {
+                if (s == '(')
+                {
+                    count++;
+                }
+                else
+                    count--;
+
+                index++;
+                if (count == -1)
+                    return index;
+            }
+            return count;
+        }
+
     }
 }
