67af6235e10de107757d6cf81109ca33ccafc943
janmaru <janmaru@gmail.com>
2020-05-17 23:54:40 +0200

    init

---
+++ src/Lib/2015.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace Mahamudra.AdventOfCode.Core
+{
+    public class TwoThousandFifteen
+    {
+        public static int DayOneWhatFloorBigO2ɛn(string input)
+        {
+            var symbols = input.ToCharArray();  //  number of char as the Length of the input string
+            var count = 0;
+            foreach (var s in symbols)
+            {
+                if (s == '(')
+                    count++;
+                else
+                    count--;
+            }
+            return count;
+        }
+
+        public static int DayOneWhatFloorv2BigO2ɛn(string input)  // BigOɛn linear x 2, hence again linear. The running time grows no faster than a constant times nnn
+        {
+            var up = input.ToCharArray().Where(x => x == ')').Sum(x => -1);
+            return input.Length + 2*up; 
+        }
+
+        public static int DayOneWhatFloorv3BigO2ɛn(string input)  // BigOɛn linear x 2, hence again linear. The running time grows no faster than a constant times nnn
+        {
+            var count = 0;
+            var separators = new string[] { "()", ")(" };
+            var sections = input.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (var s in sections)
+            {
+                if (s.First() == '(')
+                    count += s.Length;
+                else
+                    count -= s.Length;
+            }
+            return count;
+        }
+    }
+}
