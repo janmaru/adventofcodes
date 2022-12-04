17b1f4aa660d47af6971a3180d72150d37a77ffd
janmaru <janmaru@gmail.com>
2022-12-04 23:54:38 +0100

    2002 1-st day

---
+++ src/Lib/2022.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mahamudra.AdventOfCode.Core
+{
+    public class TwoThousandTwentyTwo
+    {
+        #region first day
+        public static int DayOneCalorieCounting(string[] inputs)
+        { 
+            int previous = 0;
+            int max = 0;
+            foreach (var line in inputs)
+            {
+                if (string.IsNullOrEmpty(line))
+                { 
+                    if (previous > max)
+                        max = previous;
+                    previous = 0;
+                }
+                else
+                    previous += Convert.ToInt32(line);
+            }
+            return max;
+        }
+
+        public static int DayOneCalorieCountingBestThree(string[] inputs)
+        {
+            var lista = new List<int>();
+            int previous = 0; 
+            foreach (var line in inputs)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    lista.Add(previous); 
+                    previous = 0;
+                }
+                else
+                    previous += Convert.ToInt32(line);
+            }
+            return lista.OrderByDescending(s => s).Take(3).Sum();
+        }
+        #endregion 
+    }
+}
